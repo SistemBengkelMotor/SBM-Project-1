48eb23fc162a5150f6b244a992264622f0a5b6f7
Teguh Siswanto <tsiswanto73@gmail.com>
2019-01-18 14:46:06 +0700

    test crud data KategoriOnderdil berhasil

--- src/edu/timnas/sistembengkelmotor/main/SistemBengkelMotor.java
+++ src/edu/timnas/sistembengkelmotor/main/SistemBengkelMotor.java
@@ -5,19 +5,47 @@ import edu.timnas.sistembengkelmotor.entity.KategoriOnderdil;
 import edu.timnas.sistembengkelmotor.error.KategoriOnderdilException;
 import edu.timnas.sistembengkelmotor.service.KategoriOnderdilDao;
 import java.sql.SQLException;
+import java.util.List;
 
 public class SistemBengkelMotor {
     public static void main(String[] args) throws SQLException, KategoriOnderdilException{
                 
 //        SistemBengkelMotorDatabase.getConnection();
-        
+    /* 
+//   Insert data
         KategoriOnderdilDao dao = SistemBengkelMotorDatabase.kategoriOnderdilDao();
         KategoriOnderdil katon =  new KategoriOnderdil();
         
         katon.setNamaKaton("Busi Motor");
         
         dao.insertKategoriOnderdil(katon);
+
+//    Update data
+        KategoriOnderdilDao dao = SistemBengkelMotorDatabase.kategoriOnderdilDao();
+        KategoriOnderdil katon =  dao.getKategoriOnderdil(1);
         
+        katon.setNamaKaton("Rem Motor Ninja R");
+        
+        dao.updateKategoriOnderdil(katon);
+    
+//  Tampil data
+        KategoriOnderdilDao dao = SistemBengkelMotorDatabase.kategoriOnderdilDao();
+        KategoriOnderdil katon =  dao.getKategoriOnderdil("Rem Motor Ninja R");
+                
+        List<KategoriOnderdil> list = dao.selectAllKategoriOnderdil();
+        
+        for (KategoriOnderdil kategoriOnderdil:list) {
+            System.out.println("ID   : "+kategoriOnderdil.getIdKaton());
+            System.out.println("Nama : "+kategoriOnderdil.getNamaKaton());
+        }
+
+//  Delete Data
+    */
+        KategoriOnderdilDao dao = SistemBengkelMotorDatabase.kategoriOnderdilDao();
+        
+        dao.deleteKategoriOnderdil(1);
+        
+    
     }
     
 }
